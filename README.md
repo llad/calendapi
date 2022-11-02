06aba38fb376bacbcb86c6290b3d701e0436d7aa
Mark Woodall <mwdll@icloud.com>
2022-11-02 02:02:14 +0000

    start

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 manage work week calendars
 
 ## setup
+* create repo in github
+* launch codespace for repo
 * curl https://bun.sh/install | bash
-* add exports to ~/.bashrc
+* nano ~/.bashrc, add: 
 export BUN_INSTALL="$HOME/.bun" 
 export PATH="$BUN_INSTALL/bin:$PATH" 
 * source ~/.bashrc
-* bun add hono
+* bun create hono ./
+* bun update hono
+* npm install hono -- seems like bun add/install isn't reliable yet ???
+* bun run start -- verify setup
+* ctrl-c -- to stop
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-import { Hono } from "hono";
-import { serveStatic } from 'hono/serve-static.bun';
+import { Hono } from "hono"
+import { serveStatic } from 'hono/serve-static.bun'
+import * as dayjs from 'dayjs'
 
-const port = parseInt(process.env.PORT) || 3000;
 
-const app = new Hono();
+const port = parseInt(process.env.PORT) || 3000
 
-app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }));
+const app = new Hono()
+
+app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }))
+
+
 
 app.get("/", (c) => {
-  return c.json({ message: "Hello World!" });
+  n  onst dateOne = new Date('2023-10-29')
+  const dateTwo = new Date('2023-11-02')
+c
+  r ''
 });
 
-console.log(`Running at http://localhost:${port}`);
+console.log(`Running at http://localhost:${port}`)
+
+console.log('this')
 
 export default {
   port,
